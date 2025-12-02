deedfb543e4fa302c3db0170aee2216d674a5d9f
Sebastian Rodriguez <johanrodriguez_14@hotmail.com>
2025-12-02 12:58:45 -0500

    change radial mask

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,33 +5,38 @@ import YoutubeIcon from "@/public/assets/youtube-icon.svg";
 
 export const Footer = () => {
   return (
-    <article className="fixed bottom-0 p-6 left-1/2 -translate-x-1/2">
-      <div className="flex items-center gap-6">
-        <a
-          href="https://www.instagram.com/somoslluvias?igsh=eDdrdnR4bmh6Zjdl"
-          target="_blank"
-        >
-          <InstargramIcon width={40} height={40} />
-        </a>
-        <a
-          href="https://www.facebook.com/share/17fkxYPx37/?mibextid=wwXIfr"
-          target="_blank"
-        >
-          <FacebookIcon width={50} height={50} />
-        </a>
-        <a
-          href="https://www.tiktok.com/@somoslluviass?_r=1&_t=ZS-91tEaOQTxh6"
-          target="_blank"
-        >
-          <TicktokIcon width={50} height={50} />
-        </a>
+    <article className="fixed bottom-1/4 p-6 left-1/2 -translate-x-1/2">
+      <div className=" bg-accent-foreground/80 px-4 py-3 rounded-lg">
+        <h3 className="text-xl text-background font-bold text-center mb-3">
+          Síguenos en:
+        </h3>
+        <div className="flex items-center gap-6">
+          <a
+            href="https://www.instagram.com/somoslluvias?igsh=eDdrdnR4bmh6Zjdl"
+            target="_blank"
+          >
+            <InstargramIcon width={40} height={40} />
+          </a>
+          <a
+            href="https://www.facebook.com/share/17fkxYPx37/?mibextid=wwXIfr"
+            target="_blank"
+          >
+            <FacebookIcon width={50} height={50} />
+          </a>
+          <a
+            href="https://www.tiktok.com/@somoslluviass?_r=1&_t=ZS-91tEaOQTxh6"
+            target="_blank"
+          >
+            <TicktokIcon width={50} height={50} />
+          </a>
 
-        <a
-          href="https://youtube.com/@somoslluvias?si=W-aFYaUQc85CLUtj"
-          target="_blank"
-        >
-          <YoutubeIcon width={50} height={50} />
-        </a>
+          <a
+            href="https://youtube.com/@somoslluvias?si=W-aFYaUQc85CLUtj"
+            target="_blank"
+          >
+            <YoutubeIcon width={50} height={50} />
+          </a>
+        </div>
       </div>
     </article>
   );
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,15 +7,21 @@ interface HeaderProps {
 
 export const Header = ({ className }: HeaderProps) => {
   return (
-    <header className={cn("p-5 w-full bg-transparent z-10", className)}>
-      <Image
-        width={120}
-        height={120}
-        src="/images/logo.png"
-        alt="Logo de lluvias de gloria enterna"
-        className="object-cover"
-        loading="eager"
-      />
+    <header
+      className={cn(
+        "p-5 w-full bg-transparent z-10 flex justify-center items-center",
+        className
+      )}
+    >
+      <div className="relative w-[150] h-[150px]">
+        <Image
+          fill
+          src="/images/logo.png"
+          alt="Logo de lluvias de gloria eterna"
+          className="object-contain"
+          loading="eager"
+        />
+      </div>
     </header>
   );
 };
