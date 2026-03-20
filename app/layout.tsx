e69388e218b3f5516dca2eedf974ddf07dd9c997
Sebastian Rodriguez <jhrodrig@pichincha.com>
2026-03-20 09:49:37 -0500

    add: youtube script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import { Footer } from "@/components/footer/Footer";
+import { Header } from "@/components/header/Header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-import { Header } from "@/components/header/Header";
-import { Footer } from "@/components/footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6912891959088671"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
